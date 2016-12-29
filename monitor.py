a7d416b35b7c9e2923759c395a74deb9fafdcfc6
Bright <9034785@qq.com>
2016-12-29 16:44:30 +0800

    1. Disable STD out buffer (line 11 ~ line 14) 2. Update function parameter 3. Print “*” if connection succeed 4. Default filename for both input and output

--- monitor.py
+++ monitor.py
@@ -8,40 +8,50 @@
 
 import subprocess
 from datetime import datetime
+import sys
+import os
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 
-def get_ip_list(ip_file):
+def get_ip_list(filename):
     ip_list = []
-    for line in open(ip_file):
+    for line in open(filename):
         line = line.strip('\n')
         ip_list.append(line.split(','))
     return ip_list
 
 
-def nc_test(ip_list, result_file):
+def nc_test(ip_list, filename):
     for i in ip_list:
         output = subprocess.Popen(['nc', '-w', '2', '-z', i[0], i[1]], stdout=subprocess.PIPE).communicate()[0]
         if output == '':
             result = '|' + str(datetime.now()).ljust(30) + '|' + (i[0] + ':' + i[1]).ljust(20) + '|' + 'FAILED'.ljust(10)
-            print result
-            save_to_file(result, result_file)
+            print '\n' + result
+            save_to_file(result, filename)
         else:
+            print "*",
             result = '|' + str(datetime.now()).ljust(30) + '|' + (i[0] + ':' + i[1]).ljust(20) + '|' + 'SUCCESS'.ljust(10)
-            print result
-            save_to_file(result, result_file)
+            save_to_file(result, filename)
 
 
-def save_to_file(result, result_file):
-    file_to_save = open(result_file, "a")
+def save_to_file(result, filename):
+    file_to_save = open(filename, "a")
     file_to_save.write('\n' + result)
     file_to_save.close()
 
 
 def main():
-    ip_file = raw_input('Please enter the file name to monitor: ')
-    result_file = raw_input('Please enter the fine name to save for the monitor result: ')
+    ip_file_name = raw_input('Please enter the file name to monitor[ip.txt]: ')
+    result_file_name = raw_input('Please enter the fine name to save for the monitor result[result.txt]: ')
+
+    if ip_file_name == '':
+        ip_file_name = 'ip.txt'
+    if result_file_name == '':
+        result_file_name = 'result.txt'
+
     while True:
-        nc_test(get_ip_list(ip_file), result_file)
+        nc_test(get_ip_list(ip_file_name), result_file_name)
 
 if __name__ == '__main__':
     main()
